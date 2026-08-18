8e7885aa11d4e09d0761822c62b67d26b6ff0053
Tarang Parmar <dev5.apolloinfotech@gmail.com>
2026-08-18 15:24:00 +0530

    first commit

--- admin/summaryAdvancePaymentData.php
+++ admin/summaryAdvancePaymentData.php
@@ -22,6 +22,8 @@ function summaryAdvancePaymentData($dbconn, $source)
         "ORDER BY advanceDate, c.companyname, e.isPermanent DESC, e.emp_name";
     $result = mysqli_query($dbconn, $sql);
     $dates = array();
+    $companies = array();
+    $rows = array();
     $columns = array();
     $employees = array();
     $sites = array();
@@ -30,16 +32,32 @@ function summaryAdvancePaymentData($dbconn, $source)
 
     while ($result && $row = mysqli_fetch_assoc($result)) {
         $date = $row['advanceDate'];
-        $key = $date . ':' . (int) $row['iCompanyId'];
+        //$key = $date . ':' . (int) $row['iCompanyId'];
         if (!isset($dates[$date])) {
             $dates[$date] = array();
         }
-        if (!isset($columns[$key])) {
-            $columns[$key] = array('key' => $key, 'date' => $date, 'companyId' => (int) $row['iCompanyId'], 'company' => $row['companyname']);
+        // if (!isset($columns[$key])) {
+        //     $columns[$key] = array('key' => $key, 'date' => $date, 'companyId' => (int) $row['iCompanyId'], 'company' => $row['companyname']);
+        $companyId = (int) $row['iCompanyId'];
+        $companies[$companyId] = $row['companyname'];
+        $rows[] = $row;
+    }
+
+    // Every date must repeat the complete company list.  Creating columns only
+    // from rows containing a payment made the report contract on sparse data.
+    foreach ($dates as $date => $unused) {
+        foreach ($companies as $companyId => $companyName) {
+            $key = $date . ':' . $companyId;
+            $columns[$key] = array('key' => $key, 'date' => $date, 'companyId' => $companyId, 'company' => $companyName);
             $dates[$date][] = $key;
             $columnTotals[$key] = 0;
         }
-        $sites[(int) $row['iCompanyId']] = $row['companyname'];
+        // $sites[(int) $row['iCompanyId']] = $row['companyname'];
+    }
+    $sites = $companies;
+
+    foreach ($rows as $row) {
+        $key = $row['advanceDate'] . ':' . (int) $row['iCompanyId'];
         $employeeId = (int) $row['iEmployeeId'];
         if (!isset($employees[$employeeId])) {
             $employees[$employeeId] = array(
