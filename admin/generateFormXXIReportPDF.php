e4fb2118f49e1450480bb162e37417ce02b89cda
Tarang Parmar <dev5.apolloinfotech@gmail.com>
2026-08-22 12:15:01 +0530

    first commit

--- admin/generateFormXXIReportPDF.php
+++ admin/generateFormXXIReportPDF.php
@@ -7,10 +7,7 @@ require_once('FormXXIReport.php');
 require_once('OtherReportOutput.php');
 
 try {
-    // $html = addOtherReportPdfSpacing(getFormXXIRequestData($dbconn));
-    // Form XXI's narrow legal-size register needs extra vertical breathing
-    // room so names and designations do not touch the cell borders.
-    $html = addOtherReportPdfSpacing(getFormXXIRequestData($dbconn), true, '7px 4px');
+    $html = addOtherReportPdfSpacing(getFormXXIRequestData($dbconn));
 } catch (Exception $exception) {
     ob_clean();
     exit(htmlspecialchars($exception->getMessage(), ENT_QUOTES, 'UTF-8'));
@@ -27,7 +24,8 @@ $pdf->setPrintHeader(false);
 $pdf->setPrintFooter(false);
 $pdf->SetMargins(0, 8, 0);
 $pdf->SetAutoPageBreak(true, 8);
-$pdf->SetFont('helvetica', '', 9);
+// $pdf->SetFont('helvetica', '', 9);
+$pdf->SetFont('times', '', 9);
 $pdf->AddPage('L', 'LEGAL');
 $pdf->writeHTML($html, true, false, true, false, '');
 $pdf->Output('Form-XXI-Register-of-Fine.pdf', 'I');
